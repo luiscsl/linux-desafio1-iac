a159f6788b5030bfe5ecde94212328b0f8c992dd
luiscsl <luiscslopes95@gmail.com>
2023-03-28 17:13:02 +0000

    Arquivos do primeiro Desafio Linux

---
+++ criandousuarios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Segunda tentativa para adicionar usuários..."
+
+useradd carlos -m -s /bin/bash -G GRP_ADM
+useradd maria -m -s /bin/bash -G GRP_ADM
+useradd joao -m -s /bin/bash -G GRP_ADM
+
+useradd debora -m -s /bin/bash -G GRP_VEN
+useradd sebastina -m -s /bin/bash -G GRP_VEN
+useradd roberto -m -s /bin/bash -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -G GRP_SEC
+useradd amanda -m -s /bin/bash -G GRP_SEC
+useradd rogerio -m -s /bin/bash -G GRP_SEC
+
+echo "Finalizado!"
